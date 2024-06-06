7c8589a6b530c4b9c2aae49581a0217c0af60bc2
Abdi-Suufi <abdirahman2.suufi@live.uwe.ac.uk>
2024-06-06 19:50:00 +0100

    Remove existing files

--- database stuff/@.sql
+++
@@ -1,5 +0,0 @@
--- MySQLShell dump 2.0.1  Distrib Ver 8.1.1-g2 for Win64 on x86_64 - for MySQL 8.1.0 (MySQL Community Server (GPL)), for Win64 (x86_64)
---
--- Host: localhost
--- ------------------------------------------------------
--- Server version	8.0.32
